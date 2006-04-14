3a02827d2628a7e70e4ed05fc9357b31e1d0e458
Maria Grineva <modis@ispras.ru>
2006-04-14 15:42:11 +0400

    masha darcs strange behaviour fix
    darcs-hash:20060414114211-ac417-279af7fef53a715a81f68ba9212b92179b0ac00a.gz

--- examples/applications/c/Client.c
+++ examples/applications/c/Client.c
@@ -14,7 +14,7 @@
 int main()
 {
     struct SednaConnection conn = SEDNA_CONNECTION_INITIALIZER;
-    int bytes_read, res;
+    int bytes_read, res, value;
     char buf[1024];
 
     const char* url = "localhost";
@@ -23,6 +23,9 @@ int main()
     const char* password = "MANAGER";
 
     printf("Client started.\n");
+    
+    value = SEDNA_AUTOCOMMIT_OFF;
+    res = SEsetConnectionAttr(&conn, SEDNA_ATTR_AUTOCOMMIT, (void*)&value, sizeof(int));
     
     //connecting to database "sample-db" with login "SYSTEM", password "MANAGER"
     res = SEconnect(&conn, url, db_name, login, password);
@@ -83,8 +86,10 @@ int main()
          	printf("%s", buf);
 
         }while(bytes_read > 0);
+        
     	printf("\n");
         res = SEnext(&conn);
+        printf("next returned %d\n", res);
     }
 
     if(res == SEDNA_RESULT_END) 
