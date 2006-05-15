fe748fb22bbd74e5a5848319565ccc97a4e3fe40
Alexander Boldakov <modis@ispras.ru>
2006-05-15 16:58:15 +0400

    se_exp port number
    darcs-hash:20060515125815-45750-937c7757f59e265e98f2d648f30e783d611d8072.gz

--- export/se_exp_cl.h
+++ export/se_exp_cl.h
@@ -1,6 +1,9 @@
 #include "argtable.h"
 
 #define ARG_SIZE 1000
+#define SOCKET_PORT_DIGITS_NUM 20
+#define PARSE_ERR_MSG_SIZE 1000
+#define SPROMT_LOGIN_SIZE 100
 
 const size_t narg = 11;
 
@@ -13,7 +16,7 @@ int socket_port = 5050;
 
 char command[ARG_SIZE];
 char path[ARG_SIZE];
-char host[ARG_SIZE];
+char host[ARG_SIZE + SOCKET_PORT_DIGITS_NUM + 1];
 char db_name[ARG_SIZE];
 char login[ARG_SIZE];
 char password[ARG_SIZE];
@@ -24,12 +27,12 @@ arg_rec exp_argtable[] =
 	{"--help",           NULL,       arg_lit,   &exp_l_help,                 "0",        "\t\t  display this help and exit"},
 	{"-version",         NULL,       arg_lit,   &exp_version,                "0",        "\t\t  display product version and exit"},
 	{"-verbose",        " on/off",   arg_bool,  &exp_verbose,                "off",      "\t  verbose output (default off)"},
-	{"-log",            " on/off",   arg_bool,  &exp_log,                    "on",      "\t\t  write lof file (default off)"},
+ /*	{"-log",            " on/off",   arg_bool,  &exp_log,                    "on",      "\t\t  write lof file (default off)"}, */
 	{"-host",           " host",     arg_str,   &host,                      "localhost", "\t\t  hostname of the machine with Sedna running\n\t\t\t  (default localhost)"},
- /*   {"-port-number",    " port",     arg_int,   &socket_port,               "5050",      "\t  socket listening port  (default 5050)"},*/
+    {"-port-number",    " port",     arg_int,   &socket_port,               "5050",      "\t  socket listening port  (default 5050)"},
 	{"-name",           " name",     arg_str,   &login,                     "-",         "\t\t  user name "},
 	{"-pswd",           " password", arg_str,   &password,                  "-",         "\t  user password "},
-	{NULL,              " command",  arg_str,   &command,                   "-",         "\t\t  'export' - to export data;\n\t\t\t  'restore' - to restore data;\n\t\t\t  'import' - to import data"},
+	{NULL,              " command",  arg_str,   &command,                   "-",         "\t\t  'export'-to export data/'restore'-to restore data/'import'-to import data"},
 	{NULL,              " db-name",  arg_str,   &db_name,                   "-",         "\t\t  database name"},
 	{NULL,              " path",     arg_str,   &path,                      "-",         "\t\t\t  path with exported/imported data"}
 
