34f66f2f5768ba27b709fb3db23430a46eafe069
Andrey Fomichev <modis@ispras.ru>
2006-09-12 18:13:58 +0400

    decContext bug fix
    darcs-hash:20060912141358-10a17-17d7000fc451a5982f22e022e3010dfbfe78aa5e.gz

--- kernel/tr/executor/base/decNumber/decContext.h
+++ kernel/tr/executor/base/decNumber/decContext.h
@@ -36,15 +36,30 @@
   #define DECCFULLNAME "Decimal Context Descriptor"   /* Verbose name */
   #define DECCAUTHOR   "Mike Cowlishaw"               /* Who to blame */
 
-  #if !defined(int32_t) // C99 standard integers
-    /* some standard integer types from C99 */
+  // C99 standard integers
+  /* some standard integer types from C99 */
+  #if !defined(uint8_t)     
     typedef unsigned char uint8_t;
-    typedef char int8_t;
+  #endif
+  #if !defined(int8_t)
+    typedef signed char int8_t;
+  #endif
+  #if !defined(uint16_t)
     typedef unsigned short uint16_t;
+  #endif
+  #if !defined(int16_t)
     typedef short int16_t;
+  #endif
+  #if !defined(uint32_t)
     typedef unsigned int uint32_t;
+  #endif
+  #if !defined(int32_t)
     typedef int int32_t;
+  #endif
+  #if !defined(uint64_t)
     typedef unsigned long long uint64_t;
+  #endif
+  #if !defined(int64_t)
     typedef long long int64_t;
   #endif
   #include <signal.h>              // for traps
