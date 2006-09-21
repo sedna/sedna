a08b94da6f3b774edee8940579a4592f36e1d287
Roman Pastukhov <modis@ispras.ru>
2006-09-21 18:37:52 +0400

    gcc 4.0 compilation fix
    darcs-hash:20060921143752-65e01-16074690e5b111065538ad4ced8a52d0951e7a7f.gz

--- kernel/tr/executor/base/xs_decimal_t.cpp
+++ kernel/tr/executor/base/xs_decimal_t.cpp
@@ -4,6 +4,7 @@
  */
 
 
+#include <math.h>
 
 #include "sedna.h"
 
--- kernel/tr/executor/base/xs_helper.cpp
+++ kernel/tr/executor/base/xs_helper.cpp
@@ -3,6 +3,7 @@
  * Copyright (C) 2006 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
  */
 
+#include <math.h>
 #include "sedna.h"
 
 #include "xs_helper.h"
