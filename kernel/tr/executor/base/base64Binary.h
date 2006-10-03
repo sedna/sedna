e44a6fe5b534aa1283d3e7fc0607b554b8360840
Ivan Shcheklein <modis@ispras.ru>
2006-10-03 13:32:11 +0400

    derived from xs:string types added
    darcs-hash:20061003093211-59128-c9209ce69e1a9a81945ec35661af77ce93ddd970.gz

--- kernel/tr/executor/base/base64Binary.h
+++ kernel/tr/executor/base/base64Binary.h
@@ -9,28 +9,15 @@
 #include "sedna.h"
 #include "PPBase.h"
 
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+///    Defines interface fot casting to or from xs:base64Binary type.
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 tuple_cell cast_string_type_to_xs_base64Binary(const tuple_cell &c);
 
 tuple_cell cast_base64Binary_to_hexBinary(const tuple_cell &c);
 tuple_cell cast_hexBinary_to_base64Binary(const tuple_cell &c);
-
-extern const unsigned char b64_value_to_char[64];
-extern const unsigned char b64_char_to_value[123];
-
-
-inline bool is_b64_char(unsigned char c)
-{
-    return c <= 122 && b64_char_to_value[c] != '_';
-}
-
-inline bool is_b16_char(unsigned char c)
-{
-    return is_b64_char(c) && (b64_char_to_value[c] & 3) == 0;
-}
-    
-inline bool is_b04_char(unsigned char c)
-{
-   return c == 'A'|| c == 'Q'|| c == 'g'|| c == 'w';
-}
 
 #endif
