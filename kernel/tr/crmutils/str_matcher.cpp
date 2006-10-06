5ab3f378c532cf516d97578aab911245b375ca62
Roman Pastukhov <modis@ispras.ru>
2006-10-06 16:58:37 +0400

    str_matcher update
    darcs-hash:20061006125837-65e01-139542e542acab04a44925ff3bdad6d6a9b564e0.gz

--- kernel/tr/crmutils/str_matcher.cpp
+++ kernel/tr/crmutils/str_matcher.cpp
@@ -1,29 +1,90 @@
 #include "str_matcher.h"
+#include <stdlib.h>
+#include <string.h>
 
-void StrMatcher::add_str (const char * str, const char * map_str)
+
+trie_node_t *StrMatcher::make_node()
 {
+	trie_node_t *tn = (trie_node_t *)malloc(sizeof(trie_node_t));
+	memset(tn->next, 0, sizeof(tn->next));
+	tn->res_ofs = -1;
+	tn->res_len = 0;
+	return tn;
+}
 
+trie_node_t *StrMatcher::get_node(trie_node_t *start, const char *str)
+{
+	if (*str == 0)
+		return start;
+	if (start->next[(unsigned char)*str] == NULL)
+		start->next[(unsigned char)*str] = make_node();
+	return get_node(start->next[(unsigned char)*str], str+1);
+}
+
+void StrMatcher::add_string_to_buf(const char *str, int *ofs, int *len)
+{
+	*ofs = strings_buf_used;
+	*len = 0;
+	while (*str)
+	{
+		if (strings_buf_used >= strings_buf_len)
+		{
+			strings_buf_len *= 2;
+			strings_buf = (char*)realloc(strings_buf, strings_buf_len);
+		}
+		strings_buf[strings_buf_used] = *str;
+		str++;
+		strings_buf_used++;
+	}
+	(*len) = strings_buf_used - *ofs;
+}
+
+void StrMatcher::add_str (const char * str, const char * map_str, pat_class pc)
+{
+	trie_node *node = get_node(root, str);
+	add_string_to_buf(str, &node->res_ofs, &node->res_len);
 }
 
 void StrMatcher::clear_state()
 {
+	buf_used = 0;
+	state = root;
 }
-int StrMatcher::match_next_symbol(char symb)
+int StrMatcher::match_next_symbol(char symb, pat_class pc)
 {
-	return 0;
-}
-const char * StrMatcher::get_buf()
-{
-	return 0;
-}
-unsigned int StrMatcher::get_buf_len()
-{
-	return 0;
-}
-const char* StrMatcher::get_last_match()
-{
-	return 0;
+	if (state->next[(unsigned char)symb] == NULL)
+		return 0;
+	else
+	{
+		state = state->next[(unsigned char)symb];
+		if (buf_used >= buf_len)
+		{
+			buf_len *= 2;
+			buf = (char*)realloc(buf, buf_len);
+		}
+		buf[buf_used] = symb;
+		buf_used++;
+		if (state->res_ofs == -1)
+			return -1;
+		else
+		{
+			last_match = &strings_buf[state->res_ofs];
+			last_match_len = state->res_len;
+			return 1;
+		}
+	}
 }
 StrMatcher::StrMatcher()
 {
+	strings_buf_len = 32;
+	strings_buf = (char*)malloc(strings_buf_len);
+	strings_buf_used = 0;
+	buf_len = 16;
+	buf = (char*)malloc(buf_len);
+	buf_used = 0;
+	last_match = NULL;
+	last_match_len = 0;
+	
+	root = make_node();
+	state = root;
 }
--- kernel/tr/crmutils/str_matcher.h
+++ kernel/tr/crmutils/str_matcher.h
@@ -1,15 +1,47 @@
 #ifndef _STR_MATCHER_H
 #define _STR_MATCHER_H
+
+
+enum pat_class
+{
+	pat_attribure	= 1,
+	pat_element		= 2
+};
+
+struct trie_node
+{
+	int res_ofs;
+	int res_len;
+	struct trie_node *next[256];
+};
+typedef struct trie_node trie_node_t;
+
+
 class StrMatcher 
 {
-
+private:
+	char *strings_buf;
+	int strings_buf_len;
+	int strings_buf_used;
+	char *buf;
+	int buf_len;
+	int buf_used;
+	trie_node_t *root;
+	trie_node_t *state;
+	trie_node_t *make_node();
+	void delete_trie(trie_node_t *root);
+	trie_node_t *get_node(trie_node_t *start, const char *str);
+	void add_string_to_buf(const char *str, int *ofs, int *len);
+	char *last_match;
+	int last_match_len;
 public:
-	void add_str (const char * str, const char * map_str);
+	void add_str (const char * str, const char * map_str, pat_class pc = (pat_class)-1);
 	void clear_state();
-	int match_next_symbol(char symb);//0-no matches 1-full match -1 -prefix match
-	const char * get_buf();
-	unsigned int get_buf_len();
-	const char* get_last_match();
+	int match_next_symbol(char symb, pat_class pc);//0-no matches 1-full match -1 -prefix match
+	const char * get_buf() {return buf; }
+	unsigned int get_buf_len() {return buf_used;}
+	const char* get_last_match() {return last_match; }
+	int get_last_match_len() {return last_match_len; }
 	StrMatcher();
 };
     
