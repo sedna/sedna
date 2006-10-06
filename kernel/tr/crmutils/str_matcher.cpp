7ab216c16d8ae0ebeefaa05acbb3a0b377a840bd
Roman Pastukhov <modis@ispras.ru>
2006-10-06 20:23:24 +0400

    str_matcher update (still not finished)
    darcs-hash:20061006162324-65e01-611c2d421bf6ad3ef679e0155c70afe8439f009a.gz

--- kernel/tr/crmutils/str_matcher.cpp
+++ kernel/tr/crmutils/str_matcher.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-trie_node_t *StrMatcher::make_node(trie_node_t *parent)
+trie_node_t *StrMatcher::make_node(trie_node_t *parent, char ch)
 {
 	trie_node_t *tn = (trie_node_t *)malloc(sizeof(trie_node_t));
 	memset(tn->next, 0, sizeof(tn->next));
@@ -14,6 +14,8 @@ trie_node_t *StrMatcher::make_node(trie_node_t *parent)
 		tn->len = 0;
 	else
 		tn->len = parent->len + 1;
+	tn->ch = ch;
+	tn->parent = parent;
 	return tn;
 }
 
@@ -28,7 +30,7 @@ trie_node_t *StrMatcher::get_node(trie_node_t *start, const char *str, int set_p
 	if (*str == 0)
 		return start;
 	if (start->next[(unsigned char)*str] == NULL)
-		start->next[(unsigned char)*str] = make_node(start);
+		start->next[(unsigned char)*str] = make_node(start, *str);
 	return get_node(start->next[(unsigned char)*str], str+1, set_pc);
 }
 
@@ -53,7 +55,7 @@ void StrMatcher::add_string_to_buf(const char *str, int *ofs, int *len)
 void StrMatcher::add_str (const char * str, const char * map_str, int pc)
 {
 	trie_node *node = get_node(root, str, pc);
-	add_string_to_buf(str, &node->res_ofs, &node->res_len);
+	add_string_to_buf(map_str, &node->res_ofs, &node->res_len);
 }
 
 void StrMatcher::reset()
@@ -63,7 +65,7 @@ void StrMatcher::reset()
 	
 	delete_trie(root);
 	
-	root = make_node(NULL);
+	root = make_node(NULL, 0);
 	state = root;
 }
 
@@ -72,10 +74,37 @@ trie_node_t *StrMatcher::get_ls_node(trie_node_t *node)
 	//TODO
 	return root;
 }
+ 
+#define _MIN_(a,b) (a) < (b) ? (a) : (b)
+inline void printpart(int from, int to, const char *str, int len, 
+	write_func_t write_cb, void *p, char *buf, int buf_used)
+{
+	if (from < 0)
+	{
+		write_cb(p, buf + buf_used + from, _MIN_(buf_used, 1+to-from));
+		if (to >= 0)
+			write_cb(p, str, 1+to);
+	}
+	else
+	{
+		//FIXME: assert(from >= 0)
+		//FIXME: assert(to >= from)
+		write_cb(p, str+from, 1+to-from);
+	}
+}
+
+inline void put_prefix(char *where, trie_node_t *node)
+{
+	while (node->len > 0)
+	{
+		where[node->len - 1] = node->ch;
+		node = node->parent;
+	}
+}
 
 int StrMatcher::parse(const char *str, int len, write_func_t write_cb, void *p, int pc)
 {
-	int k = 0;
+	int k = -buf_used;
 	for (int i = 0; i < len; i++)
 	{
 		while (state != root && 
@@ -88,14 +117,28 @@ int StrMatcher::parse(const char *str, int len, write_func_t write_cb, void *p, 
 			state = state->next[(unsigned char)str[i]];
 			if (state->res_ofs != -1)
 			{
-				//TODO
+				printpart(k, i - state->len, str, len, write_cb, p, buf, buf_used);
+				write_cb(p, &strings_buf[state->res_ofs], state->res_len);
+				k = i+1;
+				state = root;
 			}
 		}
+		//FIXME: assert state == root in else
 	}
-	if (k < len)
+	if (k < len - state->len)
 	{
-		//TODO: print buf + k..len-1 without last state chars, leave them in buf
+		printpart(k, len - 1 - state->len, str, len, write_cb, p, buf, buf_used);
+		k = len - state->len; //FIXME: do this only when asserts enabled
 	}
+	//FIXME: assert k == len - state->len
+	buf_used = state->len;
+	if (buf_used > buf_len)
+	{
+		while (buf_used > buf_len)
+			buf_len *= 2;
+		buf = (char*)realloc(buf, buf_len);
+	}
+	put_prefix(buf, state);
 	
 	return 0;
 }
@@ -119,6 +162,13 @@ StrMatcher::StrMatcher()
 	buf = (char*)malloc(buf_len);
 	buf_used = 0;
 	
-	root = make_node(NULL);
+	root = make_node(NULL, 0);
 	state = root;
+}
+
+StrMatcher::~StrMatcher()
+{
+	delete_trie(root);
+	free(strings_buf);
+	free(buf);
 }
