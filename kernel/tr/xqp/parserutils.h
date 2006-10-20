4d3e5d5e869a5a8e932f3b24f5639c32e5cac19f
Peter Pleshachkov <modis@ispras.ru>
2006-10-20 12:43:40 +0400

    chaecks whether start tag matches end tag
    darcs-hash:20061020084340-91adf-734368336e43cfe803e091c2ceccdb00828a91f3.gz

--- kernel/tr/xqp/parserutils.h
+++ kernel/tr/xqp/parserutils.h
@@ -3,6 +3,9 @@
  * Copyright (C) 2004 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
  */
 
+#ifndef Parser_utils_h
+#define Parser_utils_h
+
 #include "sedna.h"
 #include <string>
 
@@ -10,3 +13,6 @@ std::string erase_doublequot(char* lex_text);
 std::string replace_entity(char* lex_text, std::string find_ent, std::string replc_ent);
 std::string escape_quot(std::string text);
 std::string replace_charref(char* lex_text);
+
+
+#endif
