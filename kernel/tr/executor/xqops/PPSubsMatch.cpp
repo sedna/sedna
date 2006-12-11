5f11947c53597d279e9a70d166669576591bf855
Ivan Shcheklein <modis@ispras.ru>
2006-12-11 17:08:35 +0300

    PPSubsMatch::contains implementation in h file
    darcs-hash:20061211140835-59128-0847cc633eae9ba9d06cf3b5a48f36708f375478.gz

--- kernel/tr/executor/xqops/PPSubsMatch.cpp
+++ kernel/tr/executor/xqops/PPSubsMatch.cpp
@@ -44,16 +44,7 @@ inline void*  create_iterator(tuple_cell& t, int&l)
 		}
 	
 }
-template<class a, class b> int memcmp(a& it1, b& it2,int len)
-{
-	a i1 = it1;
-	b i2 = it2;
-	for (int i = 0; i < len; ++i, ++i1, ++i2)
-	{
-		if (*i1!=*i2)return (*i1-*i2);
-	}
-	return 0;
-}
+
 template <class a, class b> void PPSubsMatch::contains(a& it1, b& it2,int l1,int l2,tuple &t)
 {
 	int res=PPSubsMatch::contains<a,b>(it1,it2,l1,l2) ;
@@ -69,108 +60,7 @@ template <class a, class b> void PPSubsMatch::contains(a& it1, b& it2,int l1,int
 	}
 }
 
-template <class a, class b> int PPSubsMatch::contains(a& it1, b& it2,int l1,int l2)
-{
-	//2 nd argument empty
-	if (l2==0)
-	{
-		return 0;
-	}
-	//1 st argument empty
-	if (l1==0)
-	{
-		return -1;
-	}
-	//first case
-	
-	int j;
-	/*
-	for (int i=0;i<len1-len2;i++)
-	{
-	 j=0;
-	 while (c1[i+j]==c2[j]) 
-	 {
-		 if (++j==len2)
-		 {
-			t.copy(tuple_cell::atomic(true));
-			return;
-		 }
-	 }
-	}*/
-	if (l2>l1)
-	{
-		return -1;
-	}
-	//KARP_RABIN
-	int d, hx, hy, i;
-	for (d = i = 1; i < l2; ++i)
-		d = (d<<1);
-	a i1 = it1;
-	b i2 = it2;
-	for (hy = hx = i = 0; i < l2; ++i, ++i1, ++i2)
-	{
-		hx = ((hx<<1) + *i2);
-		hy = ((hy<<1) + *i1);
-	}
-   
-   j = 0;
-   i1 = it1;
-   it1 += l2;
-   a i3=it1;
-   while (j < l1-l2) 
-   {
-      if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0)
-	  {
-		return j;
-	  }
-	  hy = ((((hy) - (*i1)*d) << 1) + (*i3));      
-      ++j;
-	  ++i1;	  
-	  ++i3;
-   }
-   if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0)
-	  {
-		return j;
-	  }
-   //KNUTH-PLATT
-	/*
-	int i, kmpNext[10];
-   //prefase
-	i = 0;
-	j = kmpNext[0] = -1;
-	while (i < len2) 
-	{
-		while (j > -1 && c2[i] != c2[j])
-			j = kmpNext[j];
-		i++;
-		j++;
-		if (c2[i] == c2[j])
-			kmpNext[i] = kmpNext[j];
-		else
-			kmpNext[i] = j;
-   }
-   //algorithm
-	i = j = 0;
-   while (j < len1) 
-   {
-      while (i > -1 && c2[i] != c1[j])
-         i = kmpNext[i];
-      i++;
-      j++;
-      if (i >= len2) 
-	  {
-         t.copy(tuple_cell::atomic(false));
-		 return;
-      }
-   }
-*/
-
-
-	return -1;
-	//second case (unrealized)- really big strings
-}
-
-template int PPSubsMatch::contains<char_iterator, char_iterator>     (char_iterator&, char_iterator&, int, int);
+/*template int PPSubsMatch::contains<char_iterator, char_iterator>     (char_iterator&, char_iterator&, int, int);
 template int PPSubsMatch::contains<char_iterator, pstr_long_iterator>(char_iterator&, pstr_long_iterator&, int, int);
 template int PPSubsMatch::contains<char_iterator, estr_iterator>     (char_iterator&, estr_iterator&, int, int);
 template int PPSubsMatch::contains<pstr_long_iterator, pstr_long_iterator>(pstr_long_iterator&, pstr_long_iterator&, int, int);
@@ -178,7 +68,7 @@ template int PPSubsMatch::contains<pstr_long_iterator, char_iterator>     (pstr_
 template int PPSubsMatch::contains<pstr_long_iterator, estr_iterator>     (pstr_long_iterator&, estr_iterator&, int, int);
 template int PPSubsMatch::contains<estr_iterator, char_iterator>     (estr_iterator&, char_iterator&, int, int);
 template int PPSubsMatch::contains<estr_iterator, pstr_long_iterator>(estr_iterator&, pstr_long_iterator&, int, int);
-template int PPSubsMatch::contains<estr_iterator, estr_iterator>     (estr_iterator&, estr_iterator&, int, int);
+template int PPSubsMatch::contains<estr_iterator, estr_iterator>     (estr_iterator&, estr_iterator&, int, int);*/
 
 PPSubsMatch::~PPSubsMatch()
 {
