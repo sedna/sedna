d707d3f52b3a4b142b24a3c4b9d406a9400c78f7
Ivan Shcheklein <modis@ispras.ru>
2006-12-12 00:42:35 +0300

    PPSubsMatch slighty refactored
    darcs-hash:20061211214235-59128-150612bd437eedad23f29bb6c368bbc2c9d0cf29.gz

--- kernel/tr/executor/xqops/PPSubsMatch.h
+++ kernel/tr/executor/xqops/PPSubsMatch.h
@@ -14,6 +14,58 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// PPSubsMatch
 ///////////////////////////////////////////////////////////////////////////////
+typedef __int16 subsmatch_type;
+
+// Abstract base types
+#define sm_contains				0
+
+class PPSubsMatch : public PPIterator
+{
+protected:
+    // template <class a, class b> typedef void (PPSubsMatch::*t_comp_fun)(a& it1,b& it2,int l1,int l2,tuple &t);
+
+    // given parameters
+    PPOpIn seq1;
+	PPOpIn seq2;
+	subsmatch_type smt;
+    bool first_time;
+    // obtained parameters and local data
+    int comp_fun;
+	
+	void children(PPOpIn &_seq1_,PPOpIn &_seq2_) { _seq1_ = seq1; _seq2_ = seq2;}
+
+public:
+    virtual void open   ();
+    virtual void reopen ();
+    virtual void close  ();
+    virtual strict_fun res_fun () { return result; };
+    virtual void next   (tuple &t);
+
+    virtual PPIterator* copy(variable_context *_cxt_);
+    static bool result(PPIterator* cur, variable_context *cxt, void*& r);
+
+    PPSubsMatch(variable_context *_cxt_, PPOpIn _seq1_, PPOpIn _seq2_, subsmatch_type _smt_);
+    virtual ~PPSubsMatch();
+	
+	////////////////////////////////////////////////////////////////////////////
+    /// FACTORIES FOR Substring Matching
+    ////////////////////////////////////////////////////////////////////////////
+    template <class a, class b> static  void contains (a& it1, b& it2,int l1,int l2,tuple &t);
+    template <class a, class b> static  int  contains (a& it1, b& it2,int l1,int l2);
+	
+	static PPSubsMatch* PPFnContains(variable_context *_cxt_, 
+                                     PPOpIn _seq1_, 
+                                     PPOpIn _seq2_)
+	{
+        return new PPSubsMatch(_cxt_,_seq1_,_seq2_,sm_contains);
+	}
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// PPSubsMatch::contains Implementation
+///////////////////////////////////////////////////////////////////////////////
+
 template<class a, class b> int memcmp(a& it1, b& it2,int len)
 {
 	a i1 = it1;
@@ -25,147 +77,93 @@ template<class a, class b> int memcmp(a& it1, b& it2,int len)
 	return 0;
 }
 
-typedef __int16 subsmatch_type;
-
-// Abstract base types
-#define sm_contains				0
-
-class PPSubsMatch : public PPIterator
+template <class a, class b> static  int  PPSubsMatch::contains (a& it1, b& it2,int l1,int l2)
 {
-protected:
-   // template <class a, class b> typedef void (PPSubsMatch::*t_comp_fun)(a& it1,b& it2,int l1,int l2,tuple &t);
+    if (l2==0) return 0;
+    if (l1==0) return -1;
 
-    // given parameters
-    PPOpIn seq1;
-	PPOpIn seq2;
-	subsmatch_type smt;
-    bool first_time;
-    // obtained parameters and local data
-    int comp_fun;
-	
-	void children(PPOpIn &_seq1_,PPOpIn &_seq2_) { _seq1_ = seq1; _seq2_ = seq2;}
-public:
-    virtual void open   ();
-    virtual void reopen ();
-    virtual void close  ();
-    virtual strict_fun res_fun () { return result; };
-    virtual void next   (tuple &t) ;
-
-    virtual PPIterator* copy(variable_context *_cxt_);
-    static bool result(PPIterator* cur, variable_context *cxt, void*& r);
-
-    PPSubsMatch(variable_context *_cxt_,
-		PPOpIn _seq1_, PPOpIn _seq2_ , subsmatch_type _smt_);
-    virtual ~PPSubsMatch();
-	////////////////////////////////////////////////////////////////////////////
-    /// FACTORIES FOR Substring Matching
-    ////////////////////////////////////////////////////////////////////////////
-	template <class a, class b> static  void contains (a& it1, b& it2,int l1,int l2,tuple &t);
-	template <class a, class b> static  int  contains (a& it1, b& it2,int l1,int l2)
-    {
-    	//2 nd argument empty
-    	if (l2==0)
-    	{
-    		return 0;
-    	}
-    	//1 st argument empty
-    	if (l1==0)
-    	{
-    		return -1;
-		}
-    	//first case
-		
-    	int j;
-    	/*
-		for (int i=0;i<len1-len2;i++)
-		{
-    	 j=0;
-    	 while (c1[i+j]==c2[j]) 
-    	 {
-    		 if (++j==len2)
-    		 {
-    			t.copy(tuple_cell::atomic(true));
-    			return;
-    		 }
-    	 }
-    	}*/
-		if (l2>l1)
-    	{
-			return -1;
-    	}
-    	//KARP_RABIN
-		int d, hx, hy, i;
-		for (d = i = 1; i < l2; ++i)
-		d = (d<<1);
-    	a i1 = it1;
-    	b i2 = it2;
-    	for (hy = hx = i = 0; i < l2; ++i, ++i1, ++i2)
-    	{
-    		hx = ((hx<<1) + *i2);
-    		hy = ((hy<<1) + *i1);
-    	}
-       
-       j = 0;
-       i1 = it1;
-	   it1 += l2;
-       a i3=it1;
-	   while (j < l1-l2) 
-       {
-          if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0)
-		  {
-			return j;
-	  }
-	  hy = ((((hy) - (*i1)*d) << 1) + (*i3));      
-      ++j;
-	  ++i1;	  
-	  ++i3;
-       }
-       if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0)
-    	  {
-    		return j;
-    	  }
-       //KNUTH-PLATT
-    	/*
-    	int i, kmpNext[10];
-       //prefase
-    	i = 0;
-		j = kmpNext[0] = -1;
-    	while (i < len2) 
-		{
-    		while (j > -1 && c2[i] != c2[j])
-    			j = kmpNext[j];
-			i++;
-			j++;
-		if (c2[i] == c2[j])
-			kmpNext[i] = kmpNext[j];
-		else
-			kmpNext[i] = j;
-       }
-       //algorithm
-    	i = j = 0;
-       while (j < len1) 
-       {
-          while (i > -1 && c2[i] != c1[j])
-             i = kmpNext[i];
-          i++;
-          j++;
-          if (i >= len2) 
-		  {
-             t.copy(tuple_cell::atomic(false));
-			 return;
-          }
-       }
-	   */
-	
-
-	   return -1;
-	   //second case (unrealized)- really big strings
-    }
-	static PPSubsMatch* PPFnContains(variable_context *_cxt_, 
-            PPOpIn _seq1_, PPOpIn _seq2_)
+    int j;
+	/*
+	for (int i=0;i<len1-len2;i++)
 	{
-	 return new PPSubsMatch(_cxt_,_seq1_,_seq2_,sm_contains);
-	}
-};
+	 j=0;
+	 while (c1[i+j]==c2[j]) 
+	 {
+		 if (++j==len2)
+		 {
+			t.copy(tuple_cell::atomic(true));
+			return;
+		 }
+	 }
+	}*/
+    if (l2>l1) return -1;
+
+	//KARP_RABIN
+    int d, hx, hy, i;
+
+    for (d = i = 1; i < l2; ++i) d = (d<<1);
+
+    a i1 = it1;
+    b i2 = it2;
+    for (hy = hx = i = 0; i < l2; ++i, ++i1, ++i2)
+    {
+        hx = ((hx<<1) + *i2);
+        hy = ((hy<<1) + *i1);
+    }
+
+    j = 0;
+    i1 = it1;
+    it1 += l2;
+    a i3=it1;
+    
+    while (j < l1-l2) 
+    {
+        if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0)
+        {
+            return j;
+        }
+        hy = ((((hy) - (*i1)*d) << 1) + (*i3));      
+        ++j;
+        ++i1;	  
+        ++i3;
+    }
+    
+    if (hx == hy && memcmp<b,a>(it2, i1, l2) == 0) return j;
+   //KNUTH-PLATT
+	/*
+	int i, kmpNext[10];
+   //prefase
+	i = 0;
+	j = kmpNext[0] = -1;
+	while (i < len2) 
+	{
+		while (j > -1 && c2[i] != c2[j])
+			j = kmpNext[j];
+		i++;
+		j++;
+	if (c2[i] == c2[j])
+		kmpNext[i] = kmpNext[j];
+	else
+		kmpNext[i] = j;
+   }
+   //algorithm
+	i = j = 0;
+   while (j < len1) 
+   {
+      while (i > -1 && c2[i] != c1[j])
+         i = kmpNext[i];
+      i++;
+      j++;
+      if (i >= len2) 
+	  {
+         t.copy(tuple_cell::atomic(false));
+		 return;
+      }
+   }
+   */
+    return -1;
+   //second case (unrealized)- really big strings
+}
+
 
 #endif
