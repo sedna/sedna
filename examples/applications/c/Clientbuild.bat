8de167682451a8c2dab2422bc39100c8b66097d9
Maria Grineva <modis@ispras.ru>
2007-02-27 16:09:03 +0300

    authorization query upgrade for DDO
    darcs-hash:20070227130903-ac417-ae6dfb6d9edc916149003d2e3b2525ecdebc8c87.gz

--- examples/applications/c/Clientbuild.bat
+++ examples/applications/c/Clientbuild.bat
@@ -1,1 +1,1 @@
-cl /I ..\..\..\driver\c Client.c ..\..\..\driver\c\libsedna.lib wsock32.lib Advapi32.lib
+cl /MT /I ..\..\..\driver\c Client.c ..\..\..\driver\c\libsednamt.lib wsock32.lib Advapi32.lib
