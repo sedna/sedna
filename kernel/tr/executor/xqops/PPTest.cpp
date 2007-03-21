5997e87987713a1399b76f403d07dcf087294837
Leonid Novak <modis@ispras.ru>
2007-03-21 16:22:03 +0300

    bug 152
    darcs-hash:20070321132203-6de3f-3d31b37f71a4fb85855c1f3034fa78cbc16b1f03.gz

--- kernel/tr/executor/xqops/PPTest.cpp
+++ kernel/tr/executor/xqops/PPTest.cpp
@@ -300,7 +300,7 @@ void PPTest::checkTreeConsistency(xptr node)
 			check_blk_consistency(*((xptr*)nd.prefix));
 	//5.2 nid pstr consistency
 	CHECKP(node);
-	if (scn->textcnt&& ((t_dsc*)node_d)->data!=XNULL && is_last_shft_in_blk(((t_dsc*)node_d)->data))
+	if (scn->textcnt&& ((t_dsc*)node_d)->data!=XNULL&&((t_dsc*)node_d)->size<=PSTRMAXSIZE && is_last_shft_in_blk(((t_dsc*)node_d)->data))
 	{
 		CHECKP(node);
 		check_blk_consistency(((t_dsc*)node_d)->data);	
