00c83588a5d66622071b78bc11ed867da15d0813
Ivan Shcheklein <shcheklein@gmail.com>
2007-04-03 20:51:32 +0400

    PPPatMatch bug fixes && refactoring
    darcs-hash:20070403165132-48308-03a87fbdada85358991ecbd54c4fa773b5d94364.gz

--- kernel/tr/executor/xqops/PPPatMatch.cpp
+++ kernel/tr/executor/xqops/PPPatMatch.cpp
@@ -12,33 +12,81 @@
 
 void PPPatMatch::cf_choice(void)
 {
-switch (pmt)
+    switch (pmt)
 	{
-	case pm_match: comp_fun = &PPPatMatch::matches;break;
-	case pm_replace: comp_fun = &PPPatMatch::replace;break;
-	case pm_tokenize: 
-		{
-			comp_fun = &PPPatMatch::tokenize;break;
-			tknzr=NULL;
-		}
-		break;
+		case pm_match:    comp_fun = &PPPatMatch::matches; break;
+		case pm_replace:  comp_fun = &PPPatMatch::replace; break;
+		case pm_tokenize: comp_fun = &PPPatMatch::tokenize; break;
+		default: throw USER_EXCEPTION2(SE1003, "Impossible case in pattern matching choise function");
 	}
 }
+
+static inline const char* get_function_name(patmatch_type pmt)
+{
+    switch (pmt)
+	{
+		case pm_match:    return "fn:matches";
+		case pm_replace:  return "fn:replace";
+		case pm_tokenize: return "fn:tokenize";
+		default: throw USER_EXCEPTION2(SE1003, "Impossible case in pattern matching get name function");
+	}
+}
+
+static inline const char* get_argument_name(int arg)
+{
+    switch (arg)
+	{
+		case 1: return "first";
+		case 2: return "second";
+		case 3: return "third";
+		case 4: return "fourth";
+		default: throw USER_EXCEPTION2(SE1003, "Impossible case in pattern matching get argument name function");
+	}
+}
+
+
 PPPatMatch::PPPatMatch(dynamic_context *_cxt_,
-						 PPOpIn _seq1_, PPOpIn _seq2_,patmatch_type _pmt_):
-			PPIterator(_cxt_), seq1(_seq1_) , seq2(_seq2_),pmt(_pmt_),ch_cnt(2),tknzr(NULL)
+                       PPOpIn _seq1_, 
+                       PPOpIn _seq2_,
+                       patmatch_type _pmt_): PPIterator(_cxt_), 
+                                             seq1(_seq1_), 
+                                             seq2(_seq2_),
+                                             pmt(_pmt_),
+                                             ch_cnt(2),
+                                             tknzr(NULL)
 {
 	cf_choice();
 }
+
+
 PPPatMatch::PPPatMatch(dynamic_context *_cxt_,
-						 PPOpIn _seq1_, PPOpIn _seq2_,PPOpIn _seq3_,patmatch_type _pmt_):
-			PPIterator(_cxt_), seq1(_seq1_) , seq2(_seq2_), seq3(_seq3_),pmt(_pmt_),ch_cnt(3),tknzr(NULL)
+                       PPOpIn _seq1_, 
+                       PPOpIn _seq2_,
+                       PPOpIn _seq3_,
+                       patmatch_type _pmt_): PPIterator(_cxt_), 
+                                             seq1(_seq1_), 
+                                             seq2(_seq2_), 
+                                             seq3(_seq3_),
+                                             pmt(_pmt_),
+                                             ch_cnt(3),
+                                             tknzr(NULL)
 {
 	cf_choice();
 }
+
 PPPatMatch::PPPatMatch(dynamic_context *_cxt_,
-						 PPOpIn _seq1_, PPOpIn _seq2_,PPOpIn _seq3_,PPOpIn _seq4_,patmatch_type _pmt_):
-			PPIterator(_cxt_), seq1(_seq1_) , seq2(_seq2_), seq3(_seq3_), seq4(_seq4_),pmt(_pmt_),ch_cnt(4),tknzr(NULL)
+                       PPOpIn _seq1_,                      
+                       PPOpIn _seq2_,
+                       PPOpIn _seq3_,
+                       PPOpIn _seq4_,
+                       patmatch_type _pmt_): PPIterator(_cxt_), 
+                                             seq1(_seq1_), 
+                                             seq2(_seq2_), 
+                                             seq3(_seq3_), 
+                                             seq4(_seq4_),
+                                             pmt(_pmt_),
+                                             ch_cnt(4),
+                                             tknzr(NULL)
 {
 	cf_choice();
 }
@@ -47,61 +95,71 @@ PPPatMatch::~PPPatMatch()
 {
 	delete seq1.op;
 	seq1.op = NULL;
+	
 	delete seq2.op;
 	seq2.op = NULL;
-	if (ch_cnt>2)
+	
+	if (ch_cnt > 2)
 	{
 		delete seq3.op;
 		seq3.op = NULL;
-		if (ch_cnt>3)
+		if (ch_cnt > 3)
 		{
 			delete seq4.op;
 			seq4.op = NULL;
 		}
 	}
 }
+
 void PPPatMatch::open  ()
 {
     seq1.op->open();
 	seq2.op->open();
-	if (ch_cnt>2)
+
+	if (ch_cnt > 2)
 	{
 		seq3.op->open();
-		if (ch_cnt>3)
+		if (ch_cnt > 3)
 		{
 			seq4.op->open();
 		}
 	}
+
 	if (tknzr!=NULL)
 	{
 		delete tknzr;
 		tknzr = NULL;
 	}
     first_time = true;
- }
+}
+
 void PPPatMatch::reopen  ()
 {
     seq1.op->reopen();
 	seq2.op->reopen();
-	if (ch_cnt>2)
+	
+	if (ch_cnt > 2)
 	{
 		seq3.op->reopen();
-		if (ch_cnt>3)
+		if (ch_cnt > 3)
 		{
 			seq4.op->reopen();
 		}
 	}
+
 	if (tknzr!=NULL)
 	{
 		delete tknzr;
 		tknzr = NULL;
 	}
     first_time = true;
- }
+}
+
 void PPPatMatch::close  ()
 {
     seq1.op->close();
 	seq2.op->close();
+	
 	if (ch_cnt>2)
 	{
 		seq3.op->close();
@@ -110,16 +168,19 @@ void PPPatMatch::close  ()
 			seq4.op->close();
 		}
 	}
+	
 	if (tknzr!=NULL)
 	{
 		delete tknzr;
 		tknzr = NULL;
 	}
 }
+
 bool PPPatMatch::result(PPIterator* cur, dynamic_context *cxt, void*& r)
 {
- return true;
+    throw USER_EXCEPTION2(SE1002, "PPPatMatch::result");
 }
+
 PPIterator* PPPatMatch::copy(dynamic_context *_cxt_)
 {
 	PPPatMatch *res ;
@@ -146,102 +207,46 @@ PPIterator* PPPatMatch::copy(dynamic_context *_cxt_)
 	}
     return res;
 }
+
+static inline tuple_cell check_string_argument(tuple &t, PPOpIn &seq, bool is_empty_allowed, patmatch_type pmt, int arg_num)
+{
+    seq.op->next(t);
+    if(t.is_eos()) 
+    {
+        if(is_empty_allowed) return EMPTY_STRING_TC;
+        else throw USER_EXCEPTION2(XPTY0004, (std::string("Invalid arity of the ") + get_argument_name(arg_num) + " of " + get_function_name(pmt) + ". Argument contains empty sequence.").c_str());
+    }
+    tuple_cell res = atomize(seq.get(t));
+    if(!is_string_type(res.get_atomic_type())) 
+        throw USER_EXCEPTION2(XPTY0004, (std::string("Invalid type of the ") + get_argument_name(arg_num) + " of " + get_function_name(pmt) + " (xs_string/derived/promotable is expected).").c_str());
+    seq.op->next(t);
+    if(!t.is_eos())
+        throw USER_EXCEPTION2(XPTY0004, (std::string("Invalid arity of the ") + get_argument_name(arg_num) + " of " + get_function_name(pmt) + ". Argument contains more than one item.").c_str());
+    return res;
+}
+
 void PPPatMatch::next  (tuple &t)
 {
-    //d_printf1("1\n");
     if (first_time)
     {
+        first_time = false;
+        
+        tuple_cell t1c, t2c, t3c, t4c;
+        
+        t1c = check_string_argument(t, seq1, true,  pmt, 1);
+		t2c = check_string_argument(t, seq2, false, pmt, 2);
+		t3c.set_eos();
+		t4c.set_eos();
 
-		first_time = false;
-		seq1.op->next(t);
-		
-		//Preliminary node analysis
-		tuple_cell t1c= t.cells[0];
-		if (t.is_eos())t1c.set_eos();
-		if (!t1c.is_eos())
+		if (ch_cnt > 2)
 		{
-			t1c= atomize(t1c);
-			if (t1c.get_atomic_type()==xs_untypedAtomic)
-				t1c.set_xtype(xs_string);
-			else
-				if (!is_string_type(t1c.get_atomic_type()))
-			throw USER_EXCEPTION(XPTY0004);
-		}
-		seq2.op->next(t);
-		tuple_cell t2c= t.cells[0];
-		if (t.is_eos())t2c.set_eos();
-		if (!t2c.is_eos())
-		{
-			t2c= atomize(t2c);          
-			if (t2c.get_atomic_type()==xs_untypedAtomic)
-				t2c.set_xtype(xs_string);
-			else		
-				if (!is_string_type(t2c.get_atomic_type()))
-					throw USER_EXCEPTION(XPTY0004);
-		}
-		tuple_cell t3c;
-		tuple_cell t4c;
-		if (ch_cnt>2)
-		{
-			seq3.op->next(t);
-			t3c= t.cells[0];
-			if (t.is_eos())t3c.set_eos();
-			if (!t3c.is_eos())
-			{
-				t3c= atomize(t3c);          
-				if (t3c.get_atomic_type()==xs_untypedAtomic)
-				t3c.set_xtype(xs_string);
-				else		
-				if (!is_string_type(t3c.get_atomic_type()))
-					throw USER_EXCEPTION(XPTY0004);
-			}
-			if (ch_cnt>3)
-			{
-				seq4.op->next(t);
-				t4c= t.cells[0];
-				if (t.is_eos())t4c.set_eos();
-				if (!t4c.is_eos())
-				{
-					t4c= atomize(t4c);          
-					if (t4c.get_atomic_type()==xs_untypedAtomic)
-						t4c.set_xtype(xs_string);
-					else		
-						if (!is_string_type(t4c.get_atomic_type()))
-							throw USER_EXCEPTION(XPTY0004);
-				}
-			}
+			t3c = check_string_argument(t, seq3, false,  pmt, 3);
+			if (ch_cnt > 3)	t4c = check_string_argument(t, seq4, false,  pmt, 4);
 		}
 		
-		//apply function
-		//(this->*comp_fun)(t,&t1c,&t2c,&t3c,&t4c);
-
-		if (!t1c.is_eos())
-		{
-			seq1.op->next(t);
-			if (!t.is_eos())
-				throw USER_EXCEPTION(XPTY0004);
-		}
-		if (!t2c.is_eos())
-		{
-			seq2.op->next(t);
-			if (!t.is_eos())
-				throw USER_EXCEPTION(XPTY0004);
-		}
-		if (!t3c.is_eos())
-		{
-			seq3.op->next(t);
-			if (!t.is_eos())
-				throw USER_EXCEPTION(XPTY0004);
-		}
-		if (!t4c.is_eos())
-		{
-			seq4.op->next(t);
-			if (!t.is_eos())
-				throw USER_EXCEPTION(XPTY0004);
-		}
-		//apply function
-		(this->*comp_fun)(t,&t1c,&t2c,&t3c,&t4c);
-		if (tknzr!=NULL)
+		(this->*comp_fun)(t, &t1c, &t2c, &t3c, &t4c);
+		
+		if (tknzr != NULL)
 		{
 			tknzr->get_next_result(t);
 			if (t.is_eos())
@@ -254,7 +259,7 @@ void PPPatMatch::next  (tuple &t)
 	}
     else 
     {
-		if (tknzr!=NULL)
+		if (tknzr != NULL)
 		{
 			tknzr->get_next_result(t);
 			if (t.is_eos())
@@ -263,7 +268,6 @@ void PPPatMatch::next  (tuple &t)
 				tknzr = NULL;
 				first_time = true;
 			}
-
 		}
 		else
 		{
@@ -272,68 +276,34 @@ void PPPatMatch::next  (tuple &t)
 		}
     }
 }
+
 void PPPatMatch::tokenize (tuple &t,tuple_cell *t1,tuple_cell *t2,tuple_cell *t3,tuple_cell *t4)
 {
-	//Preliminary work
-	//1.if t1 is empty sequence
-	
-	if (t1->is_eos())
-	{
-		t1 = &EMPTY_STRING_TC;
-	}
-	//2. light atomization of second parameter
-	if (t2->is_eos())
-		throw USER_EXCEPTION(XPTY0004);
-
 	tuple_cell tc=tuple_cell::make_sure_light_atomic(*t2);
 	tuple_cell tflags = tuple_cell::eos();
 
-	if (t3 != NULL && !t3->is_eos())
-		tflags = tuple_cell::make_sure_light_atomic(*t3);
-	
-	//3. tokenizer
-	tknzr=charset_handler->tokenize( t1, &tc, &tflags);
+	if(!t3->is_eos()) tflags = tuple_cell::make_sure_light_atomic(*t3);
+
+	tknzr = charset_handler->tokenize(t1, &tc, &tflags);
 }
+
 void PPPatMatch::matches (tuple &t,tuple_cell *t1,tuple_cell *t2,tuple_cell *t3,tuple_cell *t4)
 {
-	//Preliminary work
-	//1.if t1 is empty sequence
-	
-	if (t1->is_eos())
-	{
-		t1 = &EMPTY_STRING_TC;
-	}
-	//2. light atomization of second parameter
-	if (t2->is_eos())
-		throw USER_EXCEPTION(XPTY0004);
-
 	tuple_cell tc=tuple_cell::make_sure_light_atomic(*t2);
 	tuple_cell tflags = tuple_cell::eos();
 
-	if (t3 != NULL && !t3->is_eos())
-		tflags = tuple_cell::make_sure_light_atomic(*t3);
+	if (!t3->is_eos()) tflags = tuple_cell::make_sure_light_atomic(*t3);
 	
-	//3. matcher
 	charset_handler->matches(t, t1, &tc, &tflags);
 }
+
 void PPPatMatch::replace (tuple &t,tuple_cell *t1,tuple_cell *t2,tuple_cell *t3,tuple_cell *t4)
 {
-	//Preliminary work
-	//1.if t1 is empty sequence
-	if (t1->is_eos())
-	{
-		t1 = &EMPTY_STRING_TC;
-	}
-	//2. light atomization of second parameter
-	if (t2->is_eos())
-		throw USER_EXCEPTION(XPTY0004);
-
 	tuple_cell tc=tuple_cell::make_sure_light_atomic(*t2);
 	tuple_cell tf=tuple_cell::make_sure_light_atomic(*t3);
 	tuple_cell tflags = tuple_cell::eos();
 
-	if (t4 != NULL && !t4->is_eos())
-		tflags = tuple_cell::make_sure_light_atomic(*t4);
+	if (!t4->is_eos()) tflags = tuple_cell::make_sure_light_atomic(*t4);
 	
 	charset_handler->replace(t, t1, &tc, &tf, &tflags);
 }
