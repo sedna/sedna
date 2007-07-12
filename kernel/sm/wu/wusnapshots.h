d5fe3ce9541c83507d3515bd5e05df2b374453f8
Nickolay Zavaritsky <modis@ispras.ru>
2007-07-12 14:13:57 +0400

    versions4
    darcs-hash:20070712101357-fda2f-244ba23abce3907609e5447aa1b895cf6cabb654.gz

--- kernel/sm/wu/wusnapshots.h
+++ kernel/sm/wu/wusnapshots.h
@@ -44,6 +44,7 @@ struct SnapshotsOnCheckpointInfo
 	int *activeClientIds;
 	size_t activeClientCount;
 	int state; /* 0-begin 2-inprocess 1-end */ 
+	void *userData;
 };
 
 int ShInitialise();
@@ -57,7 +58,7 @@ int ShOnRollback();
 int ShOnCommit();
 int ShAdvanceSnapshots();
 int ShOnCheckpoint(SnapshotsOnCheckpointInfo *onCheckpointInfo,
-				   void(*)(void *userData, SnapshotsVersionInfo *buf, size_t count), 
+				   int(*)(void *userData, SnapshotsVersionInfo *buf, size_t count), 
 				   void *userData);
 
 int ShNotifyCheckpointActivatedAndWaitForSnapshotAdvanced();
