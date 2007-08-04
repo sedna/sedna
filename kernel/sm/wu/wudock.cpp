23d178d2fa15de35907d7cd9d98b827d7e1a9ee1
Nickolay Zavaritsky <modis@ispras.ru>
2007-08-04 18:41:24 +0400

    versions 20
    darcs-hash:20070804144124-3c363-491cd1c25fc88b2001e400595cbe3f88f1cce262.gz

---
+++ kernel/sm/wu/wudock.cpp
@@ -0,0 +1,13 @@
+#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#include "wudock.h"
+
+XPTR WuInternaliseXptr(const xptr& v)
+{
+	return ((XPTR)UINTPTR_MAX+1)*v.layer + (uintptr_t)v.addr;
+}
+
+xptr WuExternaliseXptr(XPTR v)
+{
+	return xptr((t_layer)(v/((XPTR)UINTPTR_MAX+1)),(void*)(uintptr_t)(v));
+}
