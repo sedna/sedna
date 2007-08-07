e32224e154053dbd6f8b2ef5fe1f93dc6622b125
Peter Pleshachkov <modis@ispras.ru>
2007-08-07 17:23:02 +0400

    files added in llmgr
    darcs-hash:20070807132302-91adf-2826bd63956992c7a9c77b6558245eba1ca6b809.gz

---
+++ kernel/sm/llmgr/sm_llmgr.h
@@ -0,0 +1,14 @@
+#ifndef _SM_LLMGR_H
+#define _SM_LLMGR_H
+
+#include "sm/llmgr/llmgr_core.h"
+
+class sm_llmgr : public llmgr_core
+{
+public:
+  LONG_LSN recover_db_by_phys_records(/*const LONG_LSN& last_cp_lsn,*/ bool sync);
+  void restorePh();
+  void ll_log_checkpoint(SnapshotsOnCheckpointParams *params, SnapshotsVersion *buf, size_t count, int isGarbage);
+};
+
+#endif
