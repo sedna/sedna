5a37b5d685af5856fdaeff0d3d50cecedbb70d28
Maria Grineva <modis@ispras.ru>
2007-12-13 17:33:41 +0300

    fflush in se_term
    darcs-hash:20071213143341-ac417-b0cffb07cfc647ef8a2078e5b41b45a7b4f5ff92.gz

--- term/term_funcs.cpp
+++ term/term_funcs.cpp
@@ -11,7 +11,11 @@
 
 void term_output1(const char *buf)
 {
-    if (echo) fprintf(res_os, "%s", buf);
+    if (echo) 
+    {
+        fprintf(res_os, "%s", buf);
+        fflush(res_os);
+    }
 }
 
 void term_output2(const char *buf, const void* arg1)
@@ -21,6 +25,7 @@ void term_output2(const char *buf, const void* arg1)
     	char echo_buf[256];
     	sprintf(echo_buf,buf,arg1);
     	fprintf(res_os, "%s", echo_buf);
+        fflush(res_os);
     }
 }
 
@@ -31,6 +36,7 @@ void term_output3(const char *buf, const void* arg1, const void* arg2)
     	char echo_buf[256];
     	sprintf(echo_buf,buf,arg1,arg2);
     	fprintf(res_os, "%s", echo_buf);
+        fflush(res_os);
     }
 }
 
@@ -53,10 +59,12 @@ void term_debug_info_output(const char *msg)
         }
 
         fprintf(res_os, "\n%s", debug_indent);
+        fflush(res_os);
 
         while(i<msg_length)
         {
             fprintf(res_os, "%c", msg[i]);
+            fflush(res_os);
             if (msg[i]=='\n') fprintf(res_os, "%s", debug_indent);
             i++;
         }
@@ -75,6 +83,7 @@ int process_commandline_query()
 	
     if(res != SEDNA_SESSION_OPEN){
     	fprintf(stderr, "failed to open session \n%s\n", SEgetLastErrorMsg(&conn));
+        fflush(stderr);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
     }
 	
@@ -83,6 +92,7 @@ int process_commandline_query()
     if(res != SEDNA_BEGIN_TRANSACTION_SUCCEEDED) 
     {
     	fprintf(stderr, "failed to begin transaction\n%s\n", SEgetLastErrorMsg(&conn));
+        fflush(stderr);
         //closing session
         SEclose(&conn);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -93,6 +103,7 @@ int process_commandline_query()
     if(res == SEDNA_QUERY_FAILED) 
     {
     	fprintf(stderr, "\n%s\n", SEgetLastErrorMsg(&conn));
+        fflush(stderr);
     	//closing session
     	SEclose(&conn);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -100,6 +111,7 @@ int process_commandline_query()
     if(res == SEDNA_UPDATE_FAILED) 
     {
     	fprintf(stderr, "\n%s\n", SEgetLastErrorMsg(&conn));
+        fflush(stderr);
     	//closing session
     	SEclose(&conn);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -107,6 +119,7 @@ int process_commandline_query()
     if(res == SEDNA_BULK_LOAD_FAILED) 
     {
     	fprintf(stderr, "\n%s\n", SEgetLastErrorMsg(&conn));
+        fflush(stderr);
     	//closing session
     	SEclose(&conn);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -114,6 +127,7 @@ int process_commandline_query()
     if(res == SEDNA_ERROR) 
     {
     	fprintf(stderr, "\n%s\n%s", SEgetLastErrorMsg(&conn),query);
+        fflush(stderr);
     	//closing session
     	SEclose(&conn);
     	return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -129,6 +143,7 @@ int process_commandline_query()
         if((res == SEDNA_NEXT_ITEM_FAILED) || (res == SEDNA_ERROR))
         {
             fprintf(stderr, "\n%s\n", SEgetLastErrorMsg(&conn));
+            fflush(stderr);
             return EXIT_STATEMENT_OR_COMMAND_FAILED;
         }
 
@@ -145,6 +160,7 @@ int process_commandline_query()
     		{
 	    		buf[bytes_read] = '\0';
     			fprintf(res_os, "%s", buf);
+                fflush(res_os);
     			bytes_read = SEgetData(&conn, buf, RESULT_MSG_SIZE);
                 if (bytes_read == SEDNA_ERROR)
                 {
@@ -159,27 +175,32 @@ int process_commandline_query()
             if((res == SEDNA_NEXT_ITEM_FAILED) || (res == SEDNA_ERROR))
             {
                 fprintf(stderr, "\n%s\n", SEgetLastErrorMsg(&conn));
+                fflush(stderr);
                 return EXIT_STATEMENT_OR_COMMAND_FAILED;
             }
     	}
 		fprintf(res_os, "\n");
+        fflush(res_os);
     }
     if(res == SEDNA_UPDATE_SUCCEEDED) 
     {
 //        term_debug_info_output(); // output debug info if there was any
 
     	fprintf(res_os, "UPDATE is executed successfully\n");
+        fflush(res_os);
     }
 	if(res == SEDNA_BULK_LOAD_SUCCEEDED) 
     {
 //        term_debug_info_output(); // output debug info if there was any
 
     	fprintf(res_os, "Bulk load succeeded\n");
+        fflush(res_os);
     }
     
     if(show_time != 0)
     {
     	fprintf(stderr, "total time: %s\n",SEshowTime(&conn));
+        fflush(res_os);
     }
 	
 
@@ -190,6 +211,7 @@ int process_commandline_query()
         if(res != SEDNA_COMMIT_TRANSACTION_SUCCEEDED) 
         {
             fprintf(stderr, "failed to commit transaction \n%s\n", SEgetLastErrorMsg(&conn));
+            fflush(stderr);
             //closing session
             SEclose(&conn);
             return EXIT_STATEMENT_OR_COMMAND_FAILED;
@@ -201,6 +223,7 @@ int process_commandline_query()
     if(res != SEDNA_SESSION_CLOSED) 
     {
 	   fprintf(stderr, "session was closed with errors \n%s\n", SEgetLastErrorMsg(&conn));
+       fflush(stderr);
 	   return EXIT_STATEMENT_OR_COMMAND_FAILED;
     }
 
