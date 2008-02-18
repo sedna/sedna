79d4aaf880c76d91d8f59c2f2cf6d87d63d72769
Alexander Kalinin <modis@ispras.ru>
2008-02-18 12:35:25 +0300

    os_exceptions strange
    darcs-hash:20080218093525-54782-9fda1dbc7419a762682fa85739f6ea7617e5e0c6.gz

--- kernel/tr/vmm/os_exceptions.cpp
+++ kernel/tr/vmm/os_exceptions.cpp
@@ -226,8 +226,6 @@ void unix_sigsegv_signal_handler(int signo, siginfo_t *info, void *cxt)
 {
     //d_printf1("SEGV\n");fflush(stdout);
 
-    U_ASSERT(false);
-
 //    U_ASSERT(false);
 
     if (LAYER_ADDRESS_SPACE_START_ADDR_INT <= (int)(info->si_addr) && 
