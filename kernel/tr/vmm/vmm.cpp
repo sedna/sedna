98193c300ef5f8d2448d2289d0d97616547f83d5
Alexander Kalinin <modis@ispras.ru>
2008-02-20 14:49:02 +0300

    rcv changes
    darcs-hash:20080220114902-54782-eb77e728cc04e8e943feb36ff4bd3d2d1036eb15.gz

--- kernel/tr/vmm/vmm.cpp
+++ kernel/tr/vmm/vmm.cpp
@@ -1038,9 +1038,10 @@ void vmm_alloc_tmp_block(xptr *p) throw (SednaException)
 void vmm_delete_block(xptr p) throw (SednaException)
 {
 	// TODO: fix this interesting logic :)
+	
 	if (IS_DATA_BLOCK(p)) 
 	{
-	    CHECKP(p);
+		CHECKP(p);
 		VMM_SIGNAL_MODIFICATION(p);
 	}
 
