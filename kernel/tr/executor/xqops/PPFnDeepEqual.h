5005bd9cb48b2ce7741450475c71286e4f1e025b
Ivan Shcheklein <modis@ispras.ru>
2008-05-13 20:49:11 +0400

    fn:deep-equal fixes
    darcs-hash:20080513164911-48308-eb22c7a01d570be631d1cb4324a6c74be350ca9c.gz

--- kernel/tr/executor/xqops/PPFnDeepEqual.h
+++ kernel/tr/executor/xqops/PPFnDeepEqual.h
@@ -12,19 +12,23 @@
 
 class PPFnDeepEqual : public PPIterator
 {
-	private:
-    
-    CollationHandler* handler;
-protected:
-    // given parameters
+private:
     PPOpIn child1;
     PPOpIn child2;
 	PPOpIn collation;
 	bool first_time;
     bool eos_reached1;
 	bool eos_reached2;
+	CollationHandler* handler;
 
-    void children(PPOpIn& _child1_, PPOpIn& _child2_) { _child1_ = child1; _child2_ = child2; }
+	void children(PPOpIn& _child1_, PPOpIn& _child2_) { _child1_ = child1; _child2_ = child2; }
+    
+	bool are_nodes_deep_equal(xptr& node1,xptr& node2);
+    bool are_elements_deep_equal(xptr& node1,xptr& node2,schema_node* scm1,schema_node* scm2);
+    bool are_documents_deep_equal(xptr& node1,xptr& node2);
+    bool are_attributes_equal(xptr& node1,xptr& node2,schema_node* scm1,schema_node* scm2);
+    bool are_text_nodes_equal(xptr& node1,xptr& node2);
+    bool are_pi_equal(xptr& node1,xptr& node2);
 
 public:
     virtual void open   ();
@@ -35,22 +39,11 @@ public:
 
     virtual PPIterator* copy(dynamic_context *_cxt_);
 
-    PPFnDeepEqual(dynamic_context *_cxt_,
-             PPOpIn _child1_,
-             PPOpIn _child2_);
-	PPFnDeepEqual(dynamic_context *_cxt_,
-             PPOpIn _child1_,
-             PPOpIn _child2_,
-			 PPOpIn _collation_);
+    PPFnDeepEqual(dynamic_context *_cxt_, PPOpIn _child1_, PPOpIn _child2_);
+	PPFnDeepEqual(dynamic_context *_cxt_, PPOpIn _child1_, PPOpIn _child2_, PPOpIn _collation_);
     virtual ~PPFnDeepEqual();
 
     static bool result(PPIterator* cur, dynamic_context *cxt, void*& r);
-	bool are_nodes_deep_equal(xptr& node1,xptr& node2);
- bool are_elements_deep_equal(xptr& node1,xptr& node2);
- bool are_documents_deep_equal(xptr& node1,xptr& node2);
-bool are_attributes_equal(xptr& node1,xptr& node2,schema_node* scm1,schema_node* scm2);
- bool are_text_nodes_equal(xptr& node1,xptr& node2);
- bool are_pi_equal(xptr& node1,xptr& node2);
 };
 
 
