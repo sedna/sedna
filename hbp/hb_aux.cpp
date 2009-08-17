d2632859303ef3cdcb5c9d29d1a69d2117f68974
Alexander Kalinin <modis@ispras.ru>
2009-08-17 17:57:51 +0400

    version option added to se_hb
    Ignore-this: fde9b39147a4a8e7bebd7a4763ddfb01
    
    darcs-hash:20090817135751-54782-66587026c853addc7c770faeb893ec15f7b7b473.gz

--- hbp/hb_aux.cpp
+++ hbp/hb_aux.cpp
@@ -11,7 +11,8 @@
 #include "common/u/uprocess.h"
 #include "common/ipc_ops.h"
 
-static int hb_help = 0;                        // help message and exit
+static int hb_help = 0;          // help message and exit
+static int hb_vers = 0;                        // show version and exit
 static int hb_cmd_port;                        // command line port number
 int hb_checkpoint = 0;                         // checkpoint needed before hot-backup
 int hb_timestamp = 0;                          // we must add timestamp to directory name
@@ -20,14 +21,15 @@ char hb_db_name[SE_MAX_DB_NAME_LENGTH + 1];    // name of the db to archive
 char hb_dir_name[U_MAX_PATH];                  // name of the distance directory
 char hb_incr_mode[512];                        // increment mode (none, start, add, disable)
 int hb_port = 5050;                            // port number (priority: def.value->cfg file->command line)
-const size_t narg = 9;  // number of arguments for argtable (must be consistent with hb_argtable below)
+const size_t narg = 10;  // number of arguments for argtable (must be consistent with hb_argtable below)
 
 // command line parameters
 arg_rec hb_argtable[] =
 {
+{"-version",         NULL,          arg_lit,         &hb_vers,                 "0",   "\t\t\t   display product version and exit"},
+{"--help",           NULL,          arg_lit,         &hb_help,                "0",   "\t\t\t   display this help and exit"},
 {"-help",            NULL,          arg_lit,         &hb_help,                 "0",   "\t\t\t\t   display this help and exit"},
-{"--help",           NULL,          arg_lit,         &hb_help,                 "0",   "\t\t\t   display this help and exit"},
-{"-checkpoint",      NULL,          arg_lit,         &hb_checkpoint,           "0",   "\t\t\t   make checkpoint before backup"}, 
+{"-checkpoint",      NULL,          arg_lit,         &hb_checkpoint,           "0",   "\t\t\t   make checkpoint before backup"},
 {"-time-dir",        NULL,          arg_lit,         &hb_timestamp,            "0",   "\t\t\t   create timestamp-subdir"}, 
 {"-make-dir",        NULL,          arg_lit,         &hb_mkdir,                "0",   "\t\t\t   create directory if it doesn't exist"}, 
 {"-incr-mode",       " <increment_mode>", arg_str,   hb_incr_mode,             "none", "\t   type of the increment mode (start, add, stop)"},
@@ -54,24 +56,30 @@ void hbParseCommandLine(int argc, char **argv)
 	if (argc == 1)
 	{
 		hbPrintUsage();
-        throw USER_SOFT_EXCEPTION("\n");
+        exit(1);
 	}
 
     // parse and fetch command line
     int arg_scan_ret_val = 0; // 1 - parsed successful, 0 - there was errors
     arg_scan_ret_val = arg_scanargv(argc, argv, hb_argtable, narg, NULL, buf, NULL);
 
+    // error in parsing command line - exit
+    if (arg_scan_ret_val == 0)
+        throw USER_ENV_EXCEPTION(buf, false);
+
+    if (hb_vers == 1)
+    {
+        print_version_and_copyright("SEDNA Hot-Backup Process");
+        exit(0);
+    }
+
     // help requested - print usage info and exit
     if (hb_help == 1)
 	{
 		hbPrintUsage();
-        throw USER_SOFT_EXCEPTION("\n");
+        exit(0);
 	}
 
-    // error in parsing command line - exit
-    if (arg_scan_ret_val == 0)
-        throw USER_ENV_EXCEPTION(buf, false);
-    
     // name of the databse is not specified - exit
     if (strcmp(hb_db_name, "???") == 0)
         throw USER_ENV_EXCEPTION("unexpected command line parameters: no dbname parameter", false);
