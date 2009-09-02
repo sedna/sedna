a7c50f1fdcab46fe21c8a37808cd339e244e7438
Ivan Shcheklein <shcheklein@gmail.com>
2009-09-02 16:30:44 +0400

    fix in PPRange (first_time var initialization in open)
    Ignore-this: 759b2cd8d5119a995006b9a3b2d834c5
    
    darcs-hash:20090902123044-48308-ecc6ef239a561ccde631563c430207de022ba812.gz

--- kernel/tr/executor/xqops/PPRange.cpp
+++ kernel/tr/executor/xqops/PPRange.cpp
@@ -1,7 +1,7 @@
 /*
- * File:  PPRange.cpp
- * Copyright (C) 2004 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
- */
+* File:  PPRange.cpp
+* Copyright (C) 2004 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
+*/
 
 #include "common/sedna.h"
 
@@ -14,94 +14,95 @@ PPRange::PPRange(dynamic_context *_cxt_,
                  operation_info _info_,
                  const PPOpIn& _start_,
                  const PPOpIn& _end_) : PPIterator(_cxt_, _info_),
-                                        start_op(_start_),
-                                        end_op(_end_)
+                 start_op(_start_),
+                 end_op(_end_)
 {
 }
 
 __int64 
 PPRange::getIntFromOp(PPOpIn & op)
 {
-	tuple t(1);
-	op.op->next(t);
+    tuple t(1);
+    op.op->next(t);
 
-	if (t.is_eos())	{
-		is_emp = true;
-		return 0;
-	}
+    if (t.is_eos())	{
+        is_emp = true;
+        return 0;
+    }
 
-	if ( t.cells_number != 1 ) 
-	    throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not a single atomic value");
+    if ( t.cells_number != 1 ) 
+        throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not a single atomic value");
 
-	tuple_cell res = atomize(t.cells[0]);
-	
-	op.op->next(t);
-	
-	if (!(t.is_eos())) 
-	    throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not a single atomic value");
+    tuple_cell res = atomize(t.cells[0]);
 
-	if (res.get_atomic_type() == xs_untypedAtomic)
-		res=cast(res,xs_integer);
-	
-	if (res.get_atomic_type()!=xs_integer&&!is_derived_from_xs_integer(res.get_atomic_type()))
-		throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not an integer value");
+    op.op->next(t);
 
-	return res.get_xs_integer();
+    if (!(t.is_eos())) 
+        throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not a single atomic value");
+
+    if (res.get_atomic_type() == xs_untypedAtomic)
+        res=cast(res,xs_integer);
+
+    if (res.get_atomic_type()!=xs_integer&&!is_derived_from_xs_integer(res.get_atomic_type()))
+        throw XQUERY_EXCEPTION2(XPTY0004, "range expression argument is not an integer value");
+
+    return res.get_xs_integer();
 }
 
 PPRange::~PPRange()
 {
-	delete start_op.op;
-	start_op.op=NULL;
-	delete end_op.op;
-	end_op.op=NULL;
+    delete start_op.op;
+    start_op.op=NULL;
+    delete end_op.op;
+    end_op.op=NULL;
 }
 
 void PPRange::do_open ()
 {
-    is_emp=false;
-	start_op.op->open();
-	end_op.op->open();
+    is_emp = false;
+    first_time = true;
+    start_op.op->open();
+    end_op.op->open();
 }
 
 void PPRange::do_reopen()
 {
-	start_op.op->reopen();
-	end_op.op->reopen();
-    first_time=true;
-    is_emp=false;
+    start_op.op->reopen();
+    end_op.op->reopen();
+    first_time = true;
+    is_emp = false;
 }
 
 void PPRange::do_close()
 {
     start_op.op->close();
-	end_op.op->close();
+    end_op.op->close();
 }
 
 void PPRange::do_next(tuple &t)
 {
     if (first_time)
-	{
+    {
         start = this->getIntFromOp(start_op);
         end   = this->getIntFromOp(end_op);		
         cur = start;
         first_time=false;
-	}
-	if (is_emp || cur>end)
-	{
-		t.set_eos();
-		first_time=true;
+    }
+    if (is_emp || cur>end)
+    {
+        t.set_eos();
+        first_time=true;
         is_emp=false;
-		return;
-	}
-	t.copy(tuple_cell::atomic((__int64)cur));
+        return;
+    }
+    t.copy(tuple_cell::atomic((__int64)cur));
     cur++;
 }
 
 PPIterator* PPRange::do_copy(dynamic_context *_cxt_)
 {
     PPRange *res = se_new PPRange(_cxt_, info, start_op, end_op);
-	res->start_op.op=start_op.op->copy(_cxt_);
-	res->end_op.op=end_op.op->copy(_cxt_);
+    res->start_op.op=start_op.op->copy(_cxt_);
+    res->end_op.op=end_op.op->copy(_cxt_);
     return res;
 }
