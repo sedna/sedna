cefb72c867791823e514b319e628a18c23b29b15
Ivan Shcheklein <modis@ispras.ru>
2010-04-01 13:35:14 +0400

    new parameters in configure.sh & scripts moved from root
    Ignore-this: c8874c61fe2a3460ad881046edad85e6
    
    darcs-hash:20100401093514-48308-ec8e0af536ee53380eb63043588af4aaf6b4815b.gz

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@
 
 # This is script for making release of the Sedna system.
 # Unfortunately, it depends on the environment of MODIS group and
-# will not work in other places without modification
+# may not work in other places without modification
 
 BUILD_MACHINE=volga.ispras.ru
 
