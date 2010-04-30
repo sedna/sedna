0746c5c4e09d6853009e8f925c18ed9ebee26c72
Alexander Kalinin <allex2001@gmail.com>
2010-04-30 15:08:11 +0400

    modified configure developer script

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # common options
 ENHANCE_TERM=Readline
@@ -9,53 +9,122 @@ CMAKE_BUILD_TYPE=Debug
 DTSEARCH=ON
 SQL_CONNECTION=ON
 
-print_usage()
-{
-    echo "Usage: $(basename $0) [-nm] <path-to-build-sedna> ('nm' builds NMake files on Windows)"
-    exit 1
+error () {
+    echo "$@" > /dev/stderr
 }
 
-if [ $# -lt 1 ]; then
-    print_usage;
-fi
+usage () {
+    if [ "$1" == "error" ]; then
+        cmd=error
+    else
+        cmd=echo
+    fi
 
-SEDNA_SOURCE_DIR=$(cd $(dirname $0) && pwd)
+    $cmd "usage: `basename $0` [--help] [--nm] [--ecl]"
+    $cmd "       [--cmake=<path>] <path-to-build-sedna>"
+    $cmd ""
+    $cmd "       Options:"
+    $cmd "           --help -- this help message"
+    $cmd "           --nm   -- builds NMake Makefiles"
+    $cmd "           --ecl  -- builds Eclipse CDT4 Project"
+    $cmd "           --cmake=<path>  -- use specified cmake binary"
 
-if [ `uname | grep "CYGWIN"` ] ; then
+    if [ "$1" == "error" ]; then
+        exit 1
+    else
+        exit 0
+    fi
+}
+
+SEDNA_SOURCE_DIR=$(cd $(dirname $0) && pwd) # sedna source directory
+SEDNA_BUILD_DIR="" # sedna build directory
+CMAKE_GENERATOR="" # cmake generator to use
+CMAKE_BIN="" # cmake executable to use
+
+# need this if we run from cygwin since we use win-cmake there
+if [ `uname | grep "CYGWIN"` ]; then
    SEDNA_SOURCE_DIR=`cygpath -aw $SEDNA_SOURCE_DIR`
 fi
 
-if [ $# -gt 1 ]; then
-    case "$1" in
-    -nm)
-            CMAKE_GENERATOR="NMake Makefiles"
-            shift
+# parse options
+while [ $# -gt 0 ]; do
+    case $1 in
+        --cmake*)
+            # support both --cmake=<dir> and --cmake <dir> syntax
+            if [ "$1" != "${1##--cmake=}" ]; then
+                CMAKE_BIN=$(eval echo ${1##--cmake=}) # to expand ~
+            else
+                CMAKE_BIN=$2
+                shift
+            fi
             ;;
-    *)
-            print_usage
+        # support '-nm' as legacy
+        --nm|-nm)
+            CMAKE_GENERATOR="NMake Makefiles"
+                ;;
+        --ecl)
+            CMAKE_GENERATOR="Eclipse CDT4 - Unix Makefiles"
+                ;;
+        --help)
+            usage
+                ;;
+        *)
+            if [ -z "$SEDNA_BUILD_DIR" ]; then
+                SEDNA_BUILD_DIR=$1
+            else
+                usage error
+            fi
             ;;
     esac
+
+    shift
+done
+
+if [ -z "$SEDNA_BUILD_DIR" ]; then
+    usage error
 fi
 
-SEDNA_BUILD_DIR=$1
+if [ -z "$CMAKE_BIN" ]; then
+    CMAKE_BIN=`which cmake 2>/dev/null`
+    if [ $? -ne 0 ]; then
+        error "Cannot find cmake in PATH"
+        exit 1
+    fi
+fi
 
-mkdir -p $SEDNA_BUILD_DIR
+if [ ! -e "$CMAKE_BIN" ]; then
+    error "Cannot find cmake: $CMAKE_BIN"
+    exit 1
+fi
+
+# make build directory
+mkdir -p "$SEDNA_BUILD_DIR" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    error "cannot create build directory: $SEDNA_BUILD_DIR"
+    exit 1
+fi
 
 echo "Sedna sources in: $SEDNA_SOURCE_DIR"
 echo "Configuring Sedna in: $SEDNA_BUILD_DIR"
+echo "Using CMake: $CMAKE_BIN"
 
-if [ -z "$CMAKE_GENERATOR" ] ; then
-    CMAKE_G=""
-else
+if [ -n "$CMAKE_GENERATOR" ]; then
     CMAKE_G="-G$CMAKE_GENERATOR"
 fi
 
-pushd $SEDNA_BUILD_DIR > /dev/null 2>&1
-cmake "$CMAKE_G" -DSQL_CONNECTION=$SQL_CONNECTION -DENABLE_DTSEARCH=$DTSEARCH -DENHANCE_TERM=$ENHANCE_TERM -DEL_DEBUG=$EL_DEBUG -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DMAKE_DOC=$MAKE_DOC -DJAVA_DRIVER=$JAVA_DRIVER $SEDNA_SOURCE_DIR
+pushd "$SEDNA_BUILD_DIR" > /dev/null 2>&1
+"$CMAKE_BIN" "$CMAKE_G" -DSQL_CONNECTION=$SQL_CONNECTION \
+                        -DENABLE_DTSEARCH=$DTSEARCH \
+                        -DENHANCE_TERM=$ENHANCE_TERM \
+                        -DEL_DEBUG=$EL_DEBUG \
+                        -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+                        -DMAKE_DOC=$MAKE_DOC \
+                        -DJAVA_DRIVER=$JAVA_DRIVER \
+                        "$SEDNA_SOURCE_DIR"
 CMAKE_RET=$?
 popd  > /dev/null 2>&1
 
-if [ $CMAKE_RET -ne 0 ] ; then
-    echo "Sedna configuartion failed!!!"
+if [ $CMAKE_RET -ne 0 ]; then
+    error "Sedna configuration failed!!!"
     exit 1
 fi
