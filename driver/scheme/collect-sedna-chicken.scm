e9d03da6e114488fea94abf303a210e94b1db81b
Ivan Shcheklein <shcheklein@gmail.com>
2010-06-14 21:19:19 +0400

    F. Winkelmann's Chicken scheme driver wrapper update

--- driver/scheme/collect-sedna-chicken.scm
+++ driver/scheme/collect-sedna-chicken.scm
@@ -1,17 +1,56 @@
-
 ; File:  collect-sedna-chicken.scm
-; Copyright (C) 2004 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
+; Copyright (C) 2004 ISP RAS
+; The Institute for System Programming of the Russian Academy of Sciences
 
-; General
-(load "libs/chicken/common.scm")
-(load "libs/chicken/myenv.scm")
+; To use it in Scheme code, just evaluate
+;
+;     (import sedna)
+;
+; after loading.
 
-; Chicken natively supports SRFI-12
-;(load "libs/srfi-12.scm")
+(module sedna
+  (
+   ; Connection with a server
+   sedna:connection?
+   sedna:connect-to-database
+   sedna:disconnect-from-database
+   
+   ; Transactions
+   sedna:begin-transaction
+   sedna:end-transaction
+   
+   ; Query execution
+   sedna:next
+   sedna:result->list
+   sedna:execute-query-xml
+   sedna:execute-query
+   
+   ; Obtaining query result items as XML stream
+   sedna:input-stream?
+   sedna:read-line
+   sedna:execute-query-xml-stream
+   
+   ; Wrapper for bulk load from stream
+   sedna:bulk-load-from-xml-stream
+   
+   ; Transaction operators
+   sedna:transaction
+  )
+
+(import scheme chicken)
+(use tcp posix extras ports)
+
+(define-syntax let-values*
+  (syntax-rules ()
+    ((_ bindings . body)
+     (let*-values bindings . body))))
+
 (define exc:signal signal)
 
 ; TCP library
-(load "sedna-api/sedna-low.scm")
+(include "sedna-api/sedna-low.scm")
 
 ; Sedna API
-(load "sedna-api/sedna-api.scm")
+(include "sedna-api/sedna-api.scm")
+
+)
--- driver/scheme/libs/chicken/common.scm
+++
@@ -1,7 +0,0 @@
-;************************************************************************
-; common.scm
-; This file is the part of SSAX package (http://ssax.sourceforge.net),
-; which is in public domain.
-
-(define pp pretty-print)
-(define command-line argv)
