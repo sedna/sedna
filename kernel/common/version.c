12dc7ef444e5ff5b13f7c7c0fdf06041a05bc503
Ivan Shcheklein <shcheklein@gmail.com>
2013-07-25 19:40:31 +0400

    version copyright date updated

--- kernel/common/version.c
+++ kernel/common/version.c
@@ -1,6 +1,7 @@
 /*
- * File:  version.c
- * Copyright (C) 2004 The Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
+ * File: version.c
+ * Copyright (C) 2013 ISP RAS
+ * The Institute for System Programming of the Russian Academy of Sciences
  */
 
 
@@ -11,6 +12,6 @@ void print_version_and_copyright(const char *prog_name)
     const char* ver = SEDNA_VERSION;
     const char* build = SEDNA_BUILD;
     fprintf(stdout, "%s Version %s.%s\n", prog_name, ver, build);
-    fprintf(stdout, "Copyright (C) 2004-2010 ISP RAS and others. All rights reserved.\n"); 
-    fprintf(stdout, "See file COPYRIGHT provided with the distribution.\n");   
+    fprintf(stdout, "Copyright (C) 2004-2013 ISP RAS and others. All rights reserved.\n");
+    fprintf(stdout, "See file COPYRIGHT provided with the distribution.\n");
 }
